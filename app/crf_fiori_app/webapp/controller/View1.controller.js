a8f10da90b19bc914f31633d9859cd3ef11925ef
Shahzeb Ahmad <shahzeb.ahmad772@gmail.com>
2023-03-07 08:22:34 +0000

    First Smartfield implemented

--- app/crf_fiori_app/webapp/controller/View1.controller.js
+++ app/crf_fiori_app/webapp/controller/View1.controller.js
@@ -1,15 +1,28 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/util/MockServer",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/odata/v2/ODataModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, MockServer, JSONModel, ODataModel) {
         "use strict";
 
         return Controller.extend("ncoc.eprocurement.crffioriapp.controller.View1", {
             onInit: function () {
-
+                //create MockServer
+                var oMockServer = new MockServer({
+                        rootUri: "/crfandestimate/"
+                });
+                //Configure MockServer
+                MockServer.config({
+                    autoRespond : true,
+                    autoRespondAfter : 1000
+                });
+                //Simulate the MockServer
+                var sPath= sap.ui.require.toUrl("https://port4004-workspaces-ws-fplz2.us10.trial.applicationstudio.cloud.sap/")
             }
         });
     });
