8ce5f952021d000b11ce64bf54065652f5bb1c19
HEM CHAND <hem.chand@accenture.com>
2023-03-10 14:19:09 +0000

    Nodejs Layer created with Create On Placeholder for Create Hook Mathod

---
+++ srv/ncoceprocurementcrf-srv.js
@@ -0,0 +1,16 @@
+const cds = require("@sap/cds")
+
+
+module.exports = (srv) => {
+
+    srv.on("CREATE", "CRFHeaderFields", async function (req, next) {
+         let oPayLoad = req.data;
+         console.log(JSON.stringify(oPayLoad));
+
+         // trigger SAP Process Automation start instance API with JSON payload from CRF APP with required fields for approvers.
+        await next();
+       
+       });
+
+
+}
