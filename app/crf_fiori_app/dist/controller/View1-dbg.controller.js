f1f0d2992fcacad31e428378bf7d8f7ad0156ed2
HEM CHAND <hem.chand@accenture.com>
2023-03-10 14:07:11 +0000

    UI save functionality and successfully deployed version with HDI container created

--- app/crf_fiori_app/dist/controller/View1-dbg.controller.js
+++ app/crf_fiori_app/dist/controller/View1-dbg.controller.js
@@ -1,13 +1,11 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/core/util/MockServer",
-	"sap/ui/model/json/JSONModel",
-	"sap/ui/model/odata/v2/ODataModel"
+    "sap/m/MessageBox"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, MockServer, JSONModel, ODataModel) {
+    function (Controller, MessageBox) {
         "use strict";
 
         return Controller.extend("ncoc.eprocurement.crffioriapp.controller.View1", {
@@ -20,12 +18,47 @@ sap.ui.define([
                 var oModel = this.getOwnerComponent().getModel();
                 var oCRFHeaderFieldsSet = oModel.createEntry("CRFHeaderFields", {
                     properties: {
-                        "Directorate": "",
+                        "Directorate": "Production",
                         "Contract_Holder": "",
-                        "Contract_Title": "",  
+                        "Contract_Title": "Machin parts Contract",  
                         "Contractor_Name": "",
-                        "Contract_Number": "",
-                        "Request_Date": new Date()
+                        "Contract_Number": "BBHU78",
+                        "Request_Date": new Date(),
+                        "Contract_Strategy":"",
+                        "Contract_Currency":"",
+                        "Exchng_Rate_Date": new Date(),
+                        "Exchng_Rate_Cont_Curr":"",
+                        "Est_Cont_Value":0.00,
+                        "Est_Cont_Value_USD":0.00,
+                        "Est_Contigency":0.00,
+                        "Cost_Object":"",
+                        "Est_Local_Content":0.00,
+                        "LTPO_No":"",
+                        "Forecast_Contract_Duration":"",
+                        "Planned_Start_Date":new Date(),
+                        "Planned_End_Date": new Date(),
+                        "Optional_Extension":"",
+                        "Retrospective":"",
+                        "Description_WSM":"",
+                        "Optional_Scope":"",
+                        "Gov_Inter_Scope":"",
+                        "RCS":"",
+                        "HSSE_Risk":"High",
+                        "HSSE_ME_Influence":"Medium",
+                        "HSSE_Cat_Justification":"",
+                        "Strategy":"",
+                        "TEP":true,
+                        "TEA":true,
+                        "TE_NA":true,
+                        "TEQ":"",
+                        "Source_Justification":"",
+                        "Detail_Expln_Sourc_Just":"",
+                        "Other_Cont_Optn_Considered":"",
+                        "Detail_NCOC_Contract_Award":"",
+                        "Perf_Contractor_NCOC_Cont":"",
+                        "Consq_Non_App_NCOC_Contract_Board":"",
+                        "Commodity_Code":"",
+                        "Prequalification_Status":""
                     
                     },
                     success: function (oData, response) { }.bind(this),
@@ -37,25 +70,29 @@ sap.ui.define([
                 });
             },
             onSave : function(){
-                var oCont_Number = this.getView().byId("idContract_Number").getValue();
-                if (oCont_Number !== '') {
-                    var oModel = this.getOwnerComponent().getModel();
-                    var oCRFHeaderFieldsSet= oModel.createEntry("CRFHeaderFields",{
-                        properties: {
-                            "Directorate": this.getValue().byId("idDirectorate").getValue(),
-                            "Contract_Holder": this.getValue().byId("idContract_Holder").getValue(),
-                            "Contract_Title": this.getValue().byId("idContract_Title").getValue(),  
-                            "Contractor_Name": this.getValue().byId("idContractor_Name").getValue(),
-                            "Contract_Number": this.getValue().byId("idContract_Number").getValue(),
-                            "Request_Date": this.getValue().byId("idContract_Number").getValue(idRequest_Date)    
-                        },
-                        success: function (oData, response) { }.bind(this),
-                        error: function (oError) { }.bind(this)
-                    });   
-                } else {
-                    alert("Contract Number cannot be empty");
+              let oScreenUserInputJson = this.getView().getBindingContext().getObject();
+              var oModel = this.getView().getModel();
+              this.getView().setBusy(true);
+              if(oScreenUserInputJson.Contract_Number!='' || oScreenUserInputJson.Contract_Title!=''){
+                var oPendingChanges = oModel.getPendingChanges();
+                if (oPendingChanges) {
+                    oModel.submitChanges({
+                        success: function (oData, oResponse) {
+                            this.getView().setBusy(false);
+                            MessageBox.success("Entry Created")
+                        }.bind(this),
+                        error: function (oError) {
+                            this.getView().setBusy(false);
+                            MessageBox.error(oError)
+                        }.bind(this),
+                    });
+
                 }
-                alert("Data Saved");
+
+              }else{
+
+                MessageBox.error("Contract Number and Contract Title is Mandatory input")
+              }
 
             }
         });
